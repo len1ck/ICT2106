c6ccdd45c2936c17c654157576d2515d282d7146
weirenye <57995805+weirenye@users.noreply.github.com>
2021-02-19 14:22:27 +0800

    RuleGateway not commited
    committing rulegateway now

--- ICT2106/Gateways/RuleTableDataGateway/RuleGateway.cs
+++ ICT2106/Gateways/RuleTableDataGateway/RuleGateway.cs
@@ -16,6 +16,38 @@ namespace ICT2106.Controllers
     public class RuleGateway
     {
         private RuleDataSource dataSource;
+
+        //Test DB
+        //https://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-sql-command.html
+        public static void DBTest()
+        {
+            string connStr = "server=localhost;user=root;database=ict2106;port=3306;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT * FROM ruletable";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]+" -- "+rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+
         
     }
     
