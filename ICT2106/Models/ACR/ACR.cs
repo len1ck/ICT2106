c7b3c752998913c4d44ec0b03017ad648d7113b9
len1ck <qklim97@gmail.com>
2021-03-01 10:18:20 +0800

    added some more stuff

---
+++ ICT2106/Models/ACR/ACR.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using ICT2106.Models;
+using ICT2106.Models.RuleTableModule;
+using ICT2106.Models.ActionTableModule;
+using ICT2106.Models.ConditionTableModule;
+
+namespace ICT2106.Models.ACR
+{
+    public class ACR
+    {
+        public IRule rule = new IRule();
+        public ICondition condition = new ICondition();
+    }
+}
