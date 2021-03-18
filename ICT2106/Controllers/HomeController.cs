762b36a413f9519524057cf326de4eb63df43669
len1ck <qklim97@gmail.com>
2021-03-18 18:10:46 +0800

    added multi threading in the home controller

--- ICT2106/Controllers/HomeController.cs
+++ ICT2106/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace ICT2106.Controllers
 {
     public class HomeController : Controller
     {
+        Device dev = new Device();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,6 +23,13 @@ namespace ICT2106.Controllers
 
         public IActionResult Index()
         {
+                 // Creating and initializing threads 
+        //Thread thr1 = new Thread(() => timer.Run(30,1)); 
+        Thread thr2 = new Thread(() => setDev(30)); 
+        Thread thr3 = new Thread(() => checkDev()); 
+        //thr1.Start(); 
+        thr2.Start(); 
+        thr3.Start();
             return View();
         }
 
@@ -37,6 +47,27 @@ namespace ICT2106.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void checkDev(){
+            while (true)
+            {
+
+                System.Threading.Thread.Sleep(1000);
+                if(dev.getStatus() == "Started"){
+                    System.Console.WriteLine("Device has started");
+                }
+            }
+        }
+
+         public void setDev(int numberOfTicks){
+            while (numberOfTicks > 0)
+            {
+
+                System.Threading.Thread.Sleep(1000);
+                dev.setStatus(System.Console.ReadLine().ToString());
+                numberOfTicks--;
+            }
         }
     }
 }
