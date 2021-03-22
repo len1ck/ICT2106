77209bea40cce78e33c0dfa8ef71d28483d0567e
Xin <57608456+xin0498@users.noreply.github.com>
2021-03-22 23:05:46 +0800

    Update ConditionGateway.cs

--- ICT2106/Gateways/ConditionTableDataGateway/ConditionGateway.cs
+++ ICT2106/Gateways/ConditionTableDataGateway/ConditionGateway.cs
@@ -59,7 +59,10 @@ namespace ICT2106.Controllers
                 {
                     ICondition NewCondition = new ConditionControl();
                     NewCondition.ConditionID = Int32.Parse(rdr[0].ToString());
-                    NewCondition.RuleID = Int32.Parse(rdr[0].ToString());
+                    NewCondition.RuleID = Int32.Parse(rdr[1].ToString());
+                    NewCondition.DevID = Int32.Parse(rdr[2].ToString());
+                    NewCondition.CName = rdr[4].ToString();
+                    NewCondition.DName = rdr[5].ToString();
                     ConditionList.Add(NewCondition);
                 }
                 rdr.Close();
