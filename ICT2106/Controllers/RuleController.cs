f37f1c8ee202857017675ca34b4070f1f400a153
len1ck <reversenick@gmail.com>
2021-04-02 16:56:37 +0800

    Finished Edit and Delete

--- ICT2106/Controllers/RuleController.cs
+++ ICT2106/Controllers/RuleController.cs
@@ -29,14 +29,14 @@ namespace ICT2106.Controllers
         private IList<ActionModel> actionlist = new List<ActionModel>();
         private IList<IDevcat> catlist = new List<IDevcat>();
         private IList<IDevcond> devlist = new List<IDevcond>();
+        private IList<IMotionDetails> mdlist = new List<IMotionDetails>();
+        private IList<ITimerDetails> tdlist = new List<ITimerDetails>();
         //gateways
         private RuleGateway rg = new RuleGateway();
         private ConditionGateway cg = new ConditionGateway();
         private devcatGateway dcat = new devcatGateway();
         private devcondGateway dcon = new devcondGateway();
-        private IList<IMotionDetails> mdlist = new List<IMotionDetails>();
         private motionDetailsGateway md = new motionDetailsGateway();
-        private IList<ITimerDetails> tdlist = new List<ITimerDetails>();
         private timerDetailsGateway td = new timerDetailsGateway();
 
         public RuleController(ILogger<RuleController> logger)
@@ -52,6 +52,8 @@ namespace ICT2106.Controllers
             conditionlist = cg.GetAllCondition();
             catlist = dcat.GetAllCat();
             devlist = dcon.GetAllDev();
+            mdlist = md.GetAllMotion();
+            tdlist = td.GetAllTimer();
             foreach(IRule rule in rulelist){
                 foreach(ICondition condition in conditionlist){
                     if(rule.RuleID == condition.RuleID){
@@ -60,8 +62,10 @@ namespace ICT2106.Controllers
                 }
             }
             ViewData["CatData"] = catlist;
-            ViewData["DeviceData"] = devlist;
+            ViewData["ConData"] = devlist;
             ViewData["RuleData"] = rulelist;
+            ViewData["MotionData"] = mdlist;
+            ViewData["TimerData"] = tdlist;
             return View();
         }
         /*
@@ -96,6 +100,8 @@ namespace ICT2106.Controllers
             conditionlist = cg.GetAllCondition();
             catlist = dcat.GetAllCat();
             devlist = dcon.GetAllDev();
+            mdlist = md.GetAllMotion();
+            tdlist = td.GetAllTimer();
             foreach(IRule rule in rulelist){
                 foreach(ICondition condition in conditionlist){
                     if(rule.RuleID == condition.RuleID){
@@ -104,18 +110,90 @@ namespace ICT2106.Controllers
                 }
             }
             ViewData["CatData"] = catlist;
-            ViewData["DeviceData"] = devlist;
+            ViewData["ConData"] = devlist;
             ViewData["RuleData"] = rulelist;
+            ViewData["MotionData"] = mdlist;
+            ViewData["TimerData"] = tdlist;
+            return View("Rules");
+        }
+
+         public IActionResult RuleEdit(String ruleIDEdit, String ruleNameEdit,String conIDEdit, String MCName,String CID,String MDCID,String HP, String PP, String TCName,String TDCID,String tm)
+        {
+            int DevID = 0;
+            rg.RuleEdit(ruleIDEdit,ruleNameEdit);
+            if(CID == "1"){
+                    if (MDCID == "1")
+                    {
+                        DevID = 2;
+                    }
+                    else if(MDCID =="2"){
+                        DevID = 3;
+                    }
+                    cg.EditCondition(conIDEdit,DevID.ToString(),MCName,CID,MDCID);
+                    md.EditMotionDetails(conIDEdit,MDCID,HP,PP);
+            }
+            else if(CID == "2"){
+                    if (TDCID =="3"){
+                        DevID = 1;
+                    }
+                    else if(TDCID =="4"){
+                        DevID = 2;
+                    }
+                    cg.EditCondition(conIDEdit,DevID.ToString(),TCName,CID,TDCID);
+                    td.EditTimerDetails(conIDEdit,TDCID,tm);
+            }
+            rulelist = rg.GetAllRules();
+            conditionlist = cg.GetAllCondition();
+            catlist = dcat.GetAllCat();
+            devlist = dcon.GetAllDev();
+            mdlist = md.GetAllMotion();
+            tdlist = td.GetAllTimer();
+            foreach(IRule rule in rulelist){
+                foreach(ICondition condition in conditionlist){
+                    if(rule.RuleID == condition.RuleID){
+                        rule.Condition = condition;
+                    }
+                }
+            }
+            ViewData["CatData"] = catlist;
+            ViewData["ConData"] = devlist;
+            ViewData["RuleData"] = rulelist;
+            ViewData["MotionData"] = mdlist;
+            ViewData["TimerData"] = tdlist;
             return View("Rules");
         }
         /*
         Now just taking a string input then pass it to gateway
         */
-        public IActionResult RuleDelete(String deleteRule)
+        public IActionResult RuleDelete(String delcid, String delrid, String delcatid)
         {
-            rg.DeleteRule(deleteRule);
+            rg.RuleDelete(delrid);
+            if(delcatid == "1"){
+                    cg.DeleteCondition(delcid);
+                    md.DeleteMotion(delcid);
+            }
+            else if(delcatid == "2"){
+                    cg.DeleteCondition(delcid);
+                    td.DeleteMotion(delcid);
+            }
             rulelist = rg.GetAllRules();
+            conditionlist = cg.GetAllCondition();
+            catlist = dcat.GetAllCat();
+            devlist = dcon.GetAllDev();
+            mdlist = md.GetAllMotion();
+            tdlist = td.GetAllTimer();
+            foreach(IRule rule in rulelist){
+                foreach(ICondition condition in conditionlist){
+                    if(rule.RuleID == condition.RuleID){
+                        rule.Condition = condition;
+                    }
+                }
+            }
+            ViewData["CatData"] = catlist;
+            ViewData["ConData"] = devlist;
             ViewData["RuleData"] = rulelist;
+            ViewData["MotionData"] = mdlist;
+            ViewData["TimerData"] = tdlist;
             return View("Rules");
         }
     }
--- ICT2106/Gateways/MotionDetailsTableDataGateway/motionDetailsGateway.cs
+++ ICT2106/Gateways/MotionDetailsTableDataGateway/motionDetailsGateway.cs
@@ -19,6 +19,73 @@ namespace ICT2106.Controllers
         private List<IMotionDetails> motions = new List<IMotionDetails>();
         private MySqlConnection conn = new MySqlConnection(connStr);
 
+        public List<IMotionDetails> GetAllMotion(){
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM motionDetails";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    IMotionDetails NewMotion = new MotionDetailsControl();
+                    NewMotion.MotionDetailID = Int32.Parse(rdr[0].ToString());
+                    NewMotion.DevCondID = Int32.Parse(rdr[1].ToString());
+                    NewMotion.HumanPresence = rdr[2].ToString();
+                    NewMotion.PetPresence = rdr[3].ToString();
+                    NewMotion.CondID = Int32.Parse(rdr[4].ToString());
+                    motions.Add(NewMotion);
+                }
+                rdr.Close();
+                conn.Close();
+                return motions;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                conn.Close();
+                return motions;
+            }
+        }   
+
+        public void EditMotionDetails(String CondID, String MDCID,String HP,String PP){
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE motionDetails SET DevCondID = "+MDCID+" , HumanPresence = '"+HP+"' , PetPresence = '"+PP+"' WHERE CondID = "+CondID;
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                rdr.Close();
+                conn.Close();
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                conn.Close();
+            }
+        } 
+
+        public void DeleteMotion(string conditionID){
+            try
+            {
+                conn.Open();
+                string sql = "DELETE FROM rule.motionDetails WHERE CondID = " + conditionID;
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                rdr.Close();
+                conn.Close();
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                conn.Close();
+            }
+        } 
+
          public List<IMotionDetails> InsertMotionDetails(String dev,String HP, String PP){
             try
             {
