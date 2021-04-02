1b634b6ed51d141e32aea87e4ce3f51a90eeef50
weirenye <57995805+weirenye@users.noreply.github.com>
2021-04-02 23:23:34 +0800

    Automation Testing
    Testing for Timer is completed

--- ICT2106/Controllers/ListenController.cs
+++ ICT2106/Controllers/ListenController.cs
@@ -9,6 +9,7 @@ using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using ICT2106.Models;
 using ICT2106.Models.Listeners;
+using ICT2106.Models.ConditionTableModule;
 
 namespace ICT2106.Controllers{
     public class listenController : Controller {
@@ -23,6 +24,32 @@ namespace ICT2106.Controllers{
         {
             StartAction act = new StartAction();
             act.AppleTests();
+
+            return View("ListenTest");
+        }
+
+        public IActionResult ForDisplay()
+        {
+            StartAction act = new StartAction();
+            var AllDisplay = act.getDisplay();
+
+            return View("ListenTest", AllDisplay);
+        }
+
+        public IActionResult TerminateThread()
+        {
+            StartAction act = new StartAction();
+            act.AppleTests();
+
+            return View("ListenTest");
+        }
+        public IActionResult GetActions()
+        {
+            StartAction act = new StartAction();
+            Condition con = new Condition();
+            con.Devcat = 2;
+            con.ConditionID = 18;
+            act.ActionChecker(con);
 
             return View("ListenTest");
         }
--- ICT2106/Models/Listeners/ListenerModel.cs
+++ ICT2106/Models/Listeners/ListenerModel.cs
@@ -1,29 +1,81 @@
 using System;
 using System.Threading;
+using System.Collections.Generic;
 using ICT2106.Models.ConditionTableModule;
+using ICT2106.Controllers;
+using ICT2106.Models.TimerDetailsModule;
+using ICT2106.Models.MotionDetailsModule;
+
 
 namespace ICT2106.Models.Listeners
 {
+    class ThreadObj{
+        private int threadID;
+        private Thread threadOnly;
+
+        public Thread thisThread{
+            get {return threadOnly;}
+            set {threadOnly = value;}
+        }
+
+        public int ThreadID{
+            get {return threadID;}
+            set {threadID = value;}
+        }
+
+    }
+
+    class BothThreadList{
+        private List<ThreadObj> TimerAll = new List<ThreadObj>();
+        private List<int> MotionAll = new List<int>();
+
+        public List<ThreadObj> ATimer{
+            get {return TimerAll;}
+            set {TimerAll = value;}
+        }
+        public List<int> AMotion{
+            get {return MotionAll;}
+            set {MotionAll = value;}
+        }
+    }
+
+    
     class StartAction{
         //Start multithreading to track each condition
-        static void ActionChecker(Condition condition){
-            //creates new action tracker
-            //StartAction startAction = new StartAction();
+        public static List<ThreadObj> AllTimer = new List<ThreadObj>();
+        public static List<int> AllMotion = new List<int>();
 
+        public BothThreadList getDisplay(){
+            BothThreadList thisList = new BothThreadList();
+            thisList.ATimer = AllTimer;
+            thisList.AMotion = AllMotion;
+
+            return thisList;
+        }
+        
+
+        //public void ActionChecker(Condition condition){
+        public void ActionChecker(Condition condition){
             //Switch to see what the action is
             int DevCat = condition.Devcat;
-
+            int ConID = condition.ConditionID;
+            //int ConID = 10;
             switch(DevCat)
             {
                 //case 1 is motion
                 case 1:
                     //new Motion Thread
-                    Thread m1 = new Thread(()=>MotionFunction());
+                    AllMotion.Add(ConID);
                     break;
                 //case 2 is Timer
                 case 2:
                     //new Timer Thread
-                    Thread t1 = new Thread(()=>TimerFunction(condition));
+                    ThreadObj timer1 = new ThreadObj();
+                    Console.WriteLine("Here1");
+                    Thread t1 = new Thread(()=>TimerFunction(ConID));
+                    timer1.ThreadID = ConID;
+                    timer1.thisThread = t1;
+                    AllTimer.Add(timer1);
                     t1.Start();
                     break;
             }
@@ -32,20 +84,50 @@ namespace ICT2106.Models.Listeners
             
         }
 
-        public static void TimerFunction(Condition condition){
+        public static void AbortThread(int CondID){
+            //appleThreadList ThreadList = new appleThreadList();
+            foreach (var indiThread in AllTimer){
+                if (indiThread.ThreadID == CondID){
+                    indiThread.thisThread.Join();
+                }
+            }
+        }
+
+        //public static void TimerFunction(Condition condition){
+        //take in condition ID from where we activate
+        public static void TimerFunction(int conditionID){
+            Console.WriteLine("Here2");
             //check what kind of timer funciton it is - TBC
 
             //get TimerDetails from DB
-            int DevConID = condition.Devcon;
+            //int DevConID = condition.Devcon;
             /* <TODO>using DevConID to get Data from timerDetails(DB) using col "condID" */
-            String inpTime = "9PM"; //temp testing
+            timerDetailsGateway tdg= new timerDetailsGateway();
+            var AllTimerList = tdg.GetAllTimer();
+            String inpTime = "";
+            foreach (var i in AllTimerList){
+                Console.WriteLine(i.CondID + " == " + conditionID);
+                if(i.CondID == conditionID){
+                    inpTime = i.Time;
+                }
+            }
+            Console.WriteLine(inpTime + "Hit");
+
+            //Add error checking if unable to find
+            // if (inpTime == ""){
+            //     //Error
+            // }
+
+            //String inpTime = "9PM"; //temp testing
 
             //removing AM/PM
+            inpTime = inpTime.ToUpper();
             string newTime = inpTime.Replace("AM", "");
             newTime = inpTime.Replace("PM", "");
+            Console.WriteLine(newTime);
 
             //convert to int
-            int rawtime = Int16.Parse(newTime);
+            int rawtime = int.Parse(newTime);
 
             //checking AM/PM
             if (inpTime.Contains("PM")){
@@ -58,26 +140,31 @@ namespace ICT2106.Models.Listeners
 
             //Compare with current time
             DateTime CurTime = DateTime.Now;
-            int SecDiff = Convert.ToInt16((DT - CurTime).TotalSeconds);
-
+            Console.WriteLine(CurTime);
+            int SecDiff = Convert.ToInt32((DT - CurTime).TotalSeconds);
+            Console.WriteLine("Before:" + SecDiff);
             //If number is negative then it will activate on the next timing
             if (SecDiff < 0){
                 DT = new DateTime(DT.Year, DT.Month, DT.Day+1, DT.Hour, DT.Minute, DT.Second);
-                SecDiff = Convert.ToInt16((DT - CurTime).TotalHours);
+                Console.WriteLine(DT);
+                SecDiff = Convert.ToInt32((DT - CurTime).TotalHours); //Counting Hours, remember to change to Seconds below
+                //SecDiff = Convert.ToInt32((DT - CurTime).TotalSeconds);
             }
-
+            Console.WriteLine("After:" + SecDiff);
             //Run Countdown until update
             CountdownTimer(SecDiff);
             /* <TODO> Run code to update database once time is up*/
 
         }
 
+        public List<ThreadObj> DisTimerCondition(){
+            return AllTimer;
+        }
+
         public void AppleTests(){
             //check what kind of timer funciton it is - TBC
 
-            //get TimerDetails from DB
-            //int DevConID = condition.Devcon;
-            /* <TODO>using DevConID to get Data from timerDetails(DB) using col "condID" */
+            //Test Timing
             String inpTime = "9PM"; //temp testing
 
             //removing AM/PM
@@ -108,13 +195,21 @@ namespace ICT2106.Models.Listeners
 
             Console.WriteLine(SecDiff);
             //Run Countdown until update
-            CountdownTimer(10);
+            CountdownTimer2(5, SecDiff);
             /* <TODO> Run code to update database once time is up*/
 
         }
 
         
-
+        private static void CountdownTimer2(int time, int DeffTime){
+            while (time > 0)
+            {
+                System.Threading.Thread.Sleep(1000);
+                System.Console.WriteLine("Time Left (seconds)..." + time);;
+                time--;
+            }
+            System.Console.WriteLine(DeffTime + " Ended");
+        }
 
 
         //Function for Motion
@@ -127,7 +222,8 @@ namespace ICT2106.Models.Listeners
             while (time > 0)
             {
                 System.Threading.Thread.Sleep(1000);
-                System.Console.WriteLine("Time Left (seconds)..." + time);
+                //System.Console.WriteLine("Time Left (seconds)..." + time);
+                System.Console.WriteLine("Time Left (Hours)..." + time); //Remember to remove
                 time--;
             }
         }
